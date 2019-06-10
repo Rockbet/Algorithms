c6c2a0c66bd05b04494f5437678652c3d7f826fa
Rockbet <leonardopaesdeleon@gmail.com>
2019-06-10 19:40:39 -0300

    Create BIT 2D.cpp

---
+++ Data Structure/BIT 2D.cpp
@@ -0,0 +1,33 @@
+// Binary Indexed Tree 2D
+// Sum: O(log n * log m)
+// Update: O(log n * log m)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e3+10, maxm = 1e3+10;
+
+int n, m, tree[maxn][maxm];
+
+void update(int x, int y, int k){
+    for(int i=x; i<maxn; i+=(i&-i))
+        for(int j=y; j<maxm; j+=(j&-j))
+            tree[i][j]+=k;
+}
+
+int sum(int x, int y){
+    int s = 0;
+    for(int i=x; i>0; i-=(i&-i))
+        for(int j=y; j>0; j-=(j&-j))
+            s += tree[i][j];
+    return s;
+}
+
+int main(){
+
+    return 0;
+}
+
+
+
