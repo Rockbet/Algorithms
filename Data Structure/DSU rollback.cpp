9e3749d76ab2fb7fe99c690a37f2c504b7561cf0
Leonardo Paes <leonardopaesdeleon@gmail.com>
2020-11-11 18:53:26 -0300

    Update DSU rollback.cpp

--- Data Structure/DSU rollback.cpp
+++ Data Structure/DSU rollback.cpp
@@ -7,7 +7,7 @@ const int maxn = 5e5+10;
 struct dsu{
     int pai[maxn], sz[maxn];
     stack<edge> s;
-    int find(int x){return (pai[x] == x ? x : find(pai[x]));};
+    int find(int x){return (pai[x] == x ? x : find(pai[x]));}
     void join(int a, int b){
         a = find(a), b = find(b);
         if(a == b) return;
