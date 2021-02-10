160717bcd40e5e96e71b81e6e28cd5269b69b470
Leonardo Paes <leonardopaesdeleon@gmail.com>
2021-02-10 19:21:26 -0300

    Update Digit DP.cpp

--- DP/Digit DP.cpp
+++ DP/Digit DP.cpp
@@ -36,7 +36,7 @@ int calc(int x){
     return solve(0, 0);
 }
 int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int a, b;
     cin >> a >> b;
     cout << calc(b) - calc(a-1) << "\n";
